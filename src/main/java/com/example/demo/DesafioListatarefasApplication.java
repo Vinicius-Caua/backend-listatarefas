dd6eb5d300c52db3af715cce63ec836cf04aae0c
Vinicius Caua <vinipg1@hotmail.com>
2024-10-31 15:50:04 -0300

    commit inicial

---
+++ src/main/java/com/example/demo/DesafioListatarefasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioListatarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioListatarefasApplication.class, args);
+	}
+
+}
