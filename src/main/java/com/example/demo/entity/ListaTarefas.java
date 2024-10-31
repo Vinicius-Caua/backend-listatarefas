c1f115ca3c9c68f13effc1bbfbdc8464bc56c084
Vinicius Caua <vinipg1@hotmail.com>
2024-10-31 16:11:19 -0300

    classe de mapeamento da lista

---
+++ src/main/java/com/example/demo/entity/ListaTarefas.java
@@ -0,0 +1,76 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "Tarefas")
+public class ListaTarefas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private boolean realizada;
+    private BigDecimal custo;
+    private Date dataLimite;
+    private int ordemApresentacao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isRealizada() {
+        return realizada;
+    }
+
+    public void setRealizada(boolean realizada) {
+        this.realizada = realizada;
+    }
+
+    public BigDecimal getCusto() {
+        return custo;
+    }
+
+    public void setCusto(BigDecimal custo) {
+        this.custo = custo;
+    }
+
+    public Date getDataLimite() {
+        return dataLimite;
+    }
+
+    public void setDataLimite(Date dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+
+    public int getOrdemApresentacao() {
+        return ordemApresentacao;
+    }
+
+    public void setOrdemApresentacao(int ordemApresentacao) {
+        this.ordemApresentacao = ordemApresentacao;
+    }
+}
