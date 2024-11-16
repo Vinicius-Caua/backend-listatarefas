58138b2ac142a68ac50e6f2b10900ac9f94dd5af
Vinicius Caua <vinipg1@hotmail.com>
2024-11-16 10:45:16 -0300

    Alteração da URL frontend

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "https://litastarefas.netlify.app")
+                        .allowedOrigins("http://localhost:5173", "https://listatarefasapi.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
